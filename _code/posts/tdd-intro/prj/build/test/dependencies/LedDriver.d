ae441b5ede6cb1e0f0d5b935decb044c5a8173d6
daniel.paredes <daniel.paredes@q-loud.de>
2020-03-28 20:31:28 +0100

    new post

---
+++ _code/posts/tdd-intro/prj/build/test/dependencies/LedDriver.d
@@ -0,0 +1,1 @@
+build/test/out/c/LedDriver.o: src/LedDriver.c include/LedDriver.h
---
+++ _code/posts/tdd-intro/prj/include/LedDriver.h
@@ -0,0 +1,10 @@
+#ifndef D_LedDriver_H
+#define D_LedDriver_H
+
+#include "stdint.h"
+#include <stdlib.h>
+#include <string.h>
+
+int32_t square_x(int16_t x);
+
+#endif  /* D_LedDriver_H */
